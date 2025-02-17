fb68a515c8cfe931c4fc4ffe359b48cd2ae3b2eb
YBH <loudlyhigh7@gmail.com>
2025-02-17 20:26:29 +0900

    깃허브-유니티허브 연결 및 작업커밋 테스트
    scene 에 empty gameobj 추가, test script 추가 및 Log 남기는 코드 추가

---
+++ Tavern/Assets/test.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("This Script is for testing");
+    }
+
+    void Update()
+    {
+        
+    }
+}
