5be3498b54422980abc52337b4420d8759eeaeee
YBH <loudlyhigh7@gmail.com>
2025-03-10 05:48:30 +0900

    그릇 아이템 추가
    CookingPot - CookedUI -> 그릇 있는지 확인 후 아이템 지급, 그릇 아이템 갯수 차감
    
    1차 테스트 완료

--- Tavern/Assets/Cook/UIs/CookedScript.cs
+++ Tavern/Assets/Cook/UIs/CookedScript.cs
@@ -1,4 +1,7 @@
+using NUnit.Framework.Interfaces;
+using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,9 +16,14 @@ public class CookedScript : MonoBehaviour
 
     public GameObject cookingObj;
 
+    public Image canNotTakeFoodImage;
+    public TextMeshProUGUI needToolText;
+
+    private float errorTimer;
+
     void Start()
     {
-        
+        canNotTakeFoodImage.enabled = false;
     }
 
     void Update()
@@ -50,7 +58,67 @@ public class CookedScript : MonoBehaviour
     // 플레이어가 아이템을 챙길 수 있는지 확인 -> 도구 확인(그릇, 잔..)
     public void CheckCanTakeFood(InventoryComp playerInventroy)
     {
-        TakeFood(playerInventroy);
+        int bowlIdx = CheckBowl(playerInventroy);
+        if(bowlIdx != -1)
+        {
+            ItemBase temp = playerInventroy.CheckItem(bowlIdx);
+            if(temp != null && temp.CurrentItemData.itemCount > 1)
+            {
+                temp.CurrentItemData.itemCount -= 1;
+                playerInventroy.PopItem(bowlIdx);
+                playerInventroy.AddItem(ref temp);
+            }
+            else if(temp != null && temp.CurrentItemData.itemCount == 1)
+            {
+                playerInventroy.PopItem(bowlIdx);
+                TakeFood(playerInventroy);
+            }
+        }
+        else
+        {
+            StartErrorTimer();
+        }
+    }
+
+    private void StartErrorTimer()
+    {
+        canNotTakeFoodImage.enabled = true;
+        needToolText.text = "You Need Bowl!";
+        needToolText.enabled = true;
+
+        ResetTimer();
+        StartCoroutine(ErrorTimerCoroutine());
+    }
+
+    private System.Collections.IEnumerator ErrorTimerCoroutine()
+    {
+        while(GetErrorTime() < 2)
+        {
+            IncreaseTimer();
+            yield return null;
+        }
+
+        ResetTimer();
+        canNotTakeFoodImage.enabled = false;
+        needToolText.enabled = false;
+    }
+
+    private void ResetTimer() => errorTimer = 0f;
+    private float GetErrorTime() => errorTimer;
+    private void IncreaseTimer() => errorTimer += Time.deltaTime;
+
+    private int CheckBowl(InventoryComp playerInventroy)
+    {
+        for (int i = 0; i < playerInventroy.GetInventorySize(); i++)
+        {
+            // Bowl 체크
+            ItemBase temp = playerInventroy.CheckItem(i);
+            if (temp != null && temp.CurrentItemData.itemName == "Bowl")
+            {
+                return i;
+            }
+        }
+        return -1;
     }
 
     // 플레이어 인벤토리에 아이템 지급
